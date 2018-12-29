c916869c5158a416bd5c6beb68ec1d99208b61c0
WitalyK <o211ntrrrtt@gmail.com>
2018-12-29 18:31:36 +0300

    migr db tab products

--- app.rb
+++ app.rb
@@ -2,6 +2,12 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:pizzashop.db" #создание бд
+
+class Product < ActiveRecord::Base #создание таблицы (сущности)
+end
 
 get '/' do
 	erb :index
--- public/app.js
+++ public/app.js
@@ -3,10 +3,12 @@
 // function something() {
 // 		alert('Hello!'+ x);
 // 		x=x+1;
-// 	}
+// 	}              Глобальная переменная
 function something() {
-	window.localStorage.setItem('aaa', 555);
+	
 	var x = window.localStorage.getItem('aaa');
+	x = x*1+1;
+	window.localStorage.setItem('aaa', x);
 	alert(x);
-	// body...
+	
 }
