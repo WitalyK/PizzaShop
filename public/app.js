f542a60870e544d3b7d3f2ebe6ddedb227da13b3
WitalyK <o211ntrrrtt@GMAIL.com>
2019-01-03 11:05:03 +0300

    11

--- public/app.js
+++ public/app.js
@@ -4,6 +4,7 @@ function addtocart(id) {
 	var x = window.localStorage.getItem(key);
 	x = x*1+1;
 	window.localStorage.setItem(key, x);
+	update_order_input();
 }
 
 function enumber() {
@@ -14,5 +15,22 @@ function enumber() {
 	    var value = window.localStorage[key];
 	    sum = sum*1 + value*1;
 	}
-	alert(sum);
+	return sum;
+}
+
+function update_order_input() {
+		var orders = cart_get_orders();
+		$('#order_num').val(orders);
+
+}
+
+function cart_get_orders() {
+	var order = '';
+	var len=window.localStorage.length;
+	for(var i=0; i<len; i++) {
+	    var key = window.localStorage.key(i);
+	    var value = window.localStorage[key];
+	    order = order + key + '=' + value + ',';
+	}
+	return order;
 }
