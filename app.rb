9bc1ec5a3a4c41a624c631ae1a2c738985f58997
WitalyK <o211ntrrrtt@gmail.com>
2019-01-04 13:07:39 +0300

    add cart site

--- app.rb
+++ app.rb
@@ -19,6 +19,23 @@ get '/about' do
 end
 
 post '/cart' do
-	
-  	erb :cart
+	orders_input = params[:orders]
+	@items = parse_orders_input orders_input
+	@items.each do |item|
+		item[0] = Product.find(item[0]) #переопределяем ид объектом строкой
+	end
+
+  	erb  :cart # "Hello #{@orders.inspect}"
+end
+
+def parse_orders_input orders_input
+	s1 = orders_input.split(/,/)
+	arr = []
+	s1.each do |x|
+		s2 = x.split(/\=/)
+		s3 = s2[0].split(/_/)
+		arr2 = [s3[1],s2[1]]
+		arr.push arr2
+	end
+	return arr
 end
